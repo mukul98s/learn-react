91f154662a6c536ef513f2bc92945b63a056f4fc
Mukul Sharma <mymukul112@gmail.com>
2021-04-25 20:44:37 +0530

    Forgot to update this

--- src/App.js
+++ src/App.js
@@ -1,20 +1,65 @@
-import "./index.css";
-import Question from "./Question";
-import data from "./data";
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 
 function App() {
+  const url = "https://course-api.com/react-tabs-project";
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <h2 className="loading section">Loading...</h2>;
+  }
+
+  const user = users[index];
+
   return (
     <main>
-      <div className="section">
-        <div className="container">
-          <h3>questions and answers about login</h3>
-          <section className="info">
-            {data.map((d) => {
-              return <Question key={d.id} {...d} />;
-            })}
-          </section>
+      <section className="section">
+        <div className="title">
+          <h2>Experience</h2>
+          <div className="underline"></div>
         </div>
-      </div>
+        <div className="jobs-center" key={user.id}>
+          <div className="btn-container">
+            {users.map((item, i) => {
+              return (
+                <button
+                  key={item.id}
+                  onClick={() => setIndex(i)}
+                  className={`job-btn ${index === i && "active-btn"}`}
+                >
+                  {item.company}
+                </button>
+              );
+            })}
+          </div>
+
+          <article className="job-info">
+            <h3>{user.title}</h3>
+            <h4>{user.company}</h4>
+            <p className="job-date">{user.dates}</p>
+            {user.duties.map((duty) => {
+              return (
+                <div className="job-desc">
+                  <FaAngleDoubleRight className="job-icon" />
+                  <p>{duty}</p>
+                </div>
+              );
+            })}
+          </article>
+        </div>
+        <button className="btn">more-info</button>
+      </section>
     </main>
   );
 }
