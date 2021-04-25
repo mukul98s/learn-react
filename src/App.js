22cd724d9f4f1e07c628f1b9a34d0c3342988f77
mukul98s <mymukul112@gmail.com>
2021-04-25 15:00:55 +0530

    simple state change project

--- src/App.js
+++ src/App.js
@@ -1,54 +1,16 @@
 import "./index.css";
-import React, { useState, useEffect } from "react";
-import Loading from "./Loading";
-import Tours from "./tours";
-
-const url = "https://course-api.com/react-tours-project";
+import Review from "./review";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [tours, setTours] = useState([]);
-
-  const fetchTours = async () => {
-    setLoading(true);
-
-    try {
-      const response = await fetch(url);
-      const tours = await response.json();
-      setLoading(false);
-      setTours(tours);
-    } catch (error) {
-      setLoading(false);
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchTours();
-  }, []);
-
-  if (loading) {
-    return (
-      <main>
-        <Loading />
-      </main>
-    );
-  }
-
-  if (tours.length === 0) {
-    return (
-      <main>
-        <h2>No tourse left</h2>
-        <button onClick={fetchTours} className="btn">
-          Refresh
-        </button>
-      </main>
-    );
-  }
-
   return (
     <main>
-      <Tours tours={tours} setTours={setTours} />
+      <section className="container">
+        <div className="title">
+          <h2>Our Reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
     </main>
   );
 }
