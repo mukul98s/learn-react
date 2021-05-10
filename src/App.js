f365fa6aea4dd08e385d0d3721fa05969a94c5c9
mukul98s <mymukul112@gmail.com>
2021-05-10 18:14:30 +0530

    Nailed It

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import "./index.css";
-import Question from "./Question";
+import React, { useState } from "react";
 import data from "./data";
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(number);
+    if (number <= 0) amount = 1;
+    if (number > 8) amount = 8;
+
+    setText(data.slice(0, amount));
+  };
   return (
-    <main>
-      <div className="section">
-        <div className="container">
-          <h3>questions and answers about login</h3>
-          <section className="info">
-            {data.map((d) => {
-              return <Question key={d.id} {...d} />;
-            })}
-          </section>
-        </div>
-      </div>
-    </main>
+    <div className="section-center">
+      <h3>tired of boring lorem ipsum</h3>
+      <form className="form" onSubmit={handleSubmit}>
+        <label htmlFor="number">Paragraph</label>
+        <input
+          type="number"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+
+      <main>
+        {text.map((p) => {
+          return <p>{p}</p>;
+        })}
+      </main>
+    </div>
   );
 }
 
